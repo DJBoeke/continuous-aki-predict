6718d60580efa0c1af0149ec21340547b2cc0f38
DJBoeke <105421971+DJBoeke@users.noreply.github.com>
2023-04-16 12:38:48 +0200

    Update sedativedurations.sql

--- sql/sedativedurations.sql
+++ sql/sedativedurations.sql
@@ -2,15 +2,16 @@
 
 -- Consecutive administrations are numbered 1, 2, ...
 -- Total time on the drug can be calculated from this table
--- by grouping using ICUSTAY_ID
+-- by grouping using stay_ID
 
--- select only the ITEMIDs from the inputevents_cv table related to sedative
-DROP MATERIALIZED VIEW IF EXISTS mimiciii.SEDATIVEDURATIONS;
-CREATE MATERIALIZED VIEW mimiciii.SEDATIVEDURATIONS as
+-- select only the ITEMIDs from the inputevents table related to sedative
+set search_path to mimiciv_icu, mimiciv_hosp, mimiciv_derived;
+DROP MATERIALIZED VIEW IF EXISTS sedativedurations CASCADE;
+CREATE materialized VIEW sedativedurations as
 with io_cv as
 (
   select
-    icustay_id, charttime, itemid, stopped
+    stay_id, starttime, itemid
     -- ITEMIDs (42273, 42802) accidentally store rate in amount column
     , case
         when itemid in (42273, 42802)
@@ -22,7 +23,7 @@ with io_cv as
           then rate
         else amount
       end as amount
-  from inputevents_cv
+  from inputevents
   where itemid in
   (
     30124,30150,30308,30118,30149,30131
@@ -32,8 +33,8 @@ with io_cv as
 , io_mv as
 (
   select
-    icustay_id, linkorderid, starttime, endtime
-  from inputevents_mv io
+    stay_id, linkorderid, starttime, endtime
+  from inputevents io
   -- Subselect the sedative ITEMIDs
   where itemid in
   (
@@ -44,47 +45,47 @@ with io_cv as
 , sedativecv1 as
 (
   select
-    icustay_id, charttime, itemid
+    stay_id, starttime, itemid
     -- case statement determining whether the ITEMID is an instance of sedative usage
     , 1 as sedative
 
     -- the 'stopped' column indicates if a sedative has been disconnected
-    , max(case when stopped in ('Stopped','D/C''d') then 1
-          else 0 end) as sedative_stopped
+   -- , max(case when stopped in ('Stopped','D/C''d') then 1
+     --     else 0 end) as sedative_stopped
 
     , max(case when rate is not null then 1 else 0 end) as sedative_null
     , max(rate) as sedative_rate
     , max(amount) as sedative_amount
 
   from io_cv
-  group by icustay_id, charttime, itemid
+  group by stay_id, starttime, itemid
 )
 , sedativecv2 as
 (
   select v.*
-    , sum(sedative_null) over (partition by icustay_id, itemid order by charttime) as sedative_partition
+    , sum(sedative_null) over (partition by stay_id, itemid order by starttime) as sedative_partition
   from
     sedativecv1 v
 )
 , sedativecv3 as
 (
   select v.*
-    , first_value(sedative_rate) over (partition by icustay_id, itemid, sedative_partition order by charttime) as sedative_prevrate_ifnull
+    , first_value(sedative_rate) over (partition by stay_id, itemid, sedative_partition order by starttime) as sedative_prevrate_ifnull
   from
     sedativecv2 v
 )
 , sedativecv4 as
 (
 select
-    icustay_id
-    , charttime
+    stay_id
+    , starttime
     , itemid
-    -- , (CHARTTIME - (LAG(CHARTTIME, 1) OVER (partition by icustay_id, vaso order by charttime))) AS delta
+    -- , (starttime - (LAG(starttime, 1) OVER (partition by stay_id, vaso order by starttime))) AS delta
 
     , sedative
     , sedative_rate
     , sedative_amount
-    , sedative_stopped
+    --, sedative_stopped
     , sedative_prevrate_ifnull
 
     -- We define start time here
@@ -96,8 +97,8 @@ select
           LAG(sedative_prevrate_ifnull,1)
           OVER
           (
-          partition by icustay_id, itemid, sedative, sedative_null
-          order by charttime
+          partition by stay_id, itemid, sedative, sedative_null
+          order by starttime
           )
           is null
           then 1
@@ -108,8 +109,8 @@ select
           LAG(sedative_prevrate_ifnull,1)
           OVER
           (
-          partition by icustay_id, itemid, sedative
-          order by charttime
+          partition by stay_id, itemid, sedative
+          order by starttime
           )
           = 0
           then 0
@@ -121,8 +122,8 @@ select
           LAG(sedative_prevrate_ifnull,1)
           OVER
           (
-          partition by icustay_id, itemid, sedative
-          order by charttime
+          partition by stay_id, itemid, sedative
+          order by starttime
           )
           = 0
           then 0
@@ -131,23 +132,16 @@ select
         when LAG(sedative_prevrate_ifnull,1)
           OVER
           (
-          partition by icustay_id, itemid, sedative
-          order by charttime
+          partition by stay_id, itemid, sedative
+          order by starttime
           ) = 0
           then 1
 
         -- If the last recorded sedative was D/C'd, newsedative = 1
-        when
-          LAG(sedative_stopped,1)
-          OVER
-          (
-          partition by icustay_id, itemid, sedative
-          order by charttime
-          )
-          = 1 then 1
+        
 
         -- ** not sure if the below is needed
-        --when (CHARTTIME - (LAG(CHARTTIME, 1) OVER (partition by icustay_id, vaso order by charttime))) > (interval '4 hours') then 1
+        --when (starttime - (LAG(starttime, 1) OVER (partition by stay_id, vaso order by starttime))) > (interval '4 hours') then 1
       else null
       end as sedative_start
 
@@ -158,7 +152,7 @@ FROM
 , sedativecv5 as
 (
   select v.*
-    , SUM(sedative_start) OVER (partition by icustay_id, itemid, sedative order by charttime) as sedative_first
+    , SUM(sedative_start) OVER (partition by stay_id, itemid, sedative order by starttime) as sedative_first
 FROM
   sedativecv4 v
 )
@@ -171,8 +165,8 @@ FROM
           then null
 
         -- If the recorded sedative was D/C'd, this is an end time
-        when sedative_stopped = 1
-          then sedative_first
+        --when sedative_stopped = 1
+         -- then sedative_first
 
         -- If the rate is zero, this is the end time
         when sedative_rate = 0
@@ -182,11 +176,11 @@ FROM
         -- this captures patients who die/are discharged while on sedatives
         -- in principle, this could add an extra end time for the sedative
         -- however, since we later group on sedative_start, any extra end times are ignored
-        when LEAD(CHARTTIME,1)
+        when LEAD(starttime,1)
           OVER
           (
-          partition by icustay_id, itemid, sedative
-          order by charttime
+          partition by stay_id, itemid, sedative
+          order by starttime
           ) is null
           then sedative_first
 
@@ -197,34 +191,34 @@ FROM
 
 -- -- if you want to look at the results of the table before grouping:
 -- select
---   icustay_id, charttime, vaso, vaso_rate, vaso_amount
+--   stay_id, starttime, vaso, vaso_rate, vaso_amount
 --     , case when vaso_stopped = 1 then 'Y' else '' end as stopped
 --     , vaso_start
 --     , vaso_first
 --     , vaso_stop
--- from vasocv6 order by charttime;
+-- from vasocv6 order by starttime;
 
 
 , sedativecv as
 (
 -- below groups together sedative administrations into groups
 select
-  icustay_id
+  stay_id
   , itemid
   -- the first non-null rate is considered the starttime
-  , min(case when sedative_rate is not null then charttime else null end) as starttime
+  , min(case when sedative_rate is not null then starttime else null end) as starttime
   -- the *first* time the first/last flags agree is the stop time for this duration
-  , min(case when sedative_first = sedative_stop then charttime else null end) as endtime
+  , min(case when sedative_first = sedative_stop then starttime else null end) as endtime
 from sedativecv6
 where
   sedative_first is not null -- bogus data
 and
   sedative_first != 0 -- sometimes *only* a rate of 0 appears, i.e. the drug is never actually delivered
 and
-  icustay_id is not null -- there are data for "floating" admissions, we don't worry about these
-group by icustay_id, itemid, sedative_first
+  stay_id is not null -- there are data for "floating" admissions, we don't worry about these
+group by stay_id, itemid, sedative_first
 having -- ensure start time is not the same as end time
- min(charttime) != min(case when sedative_first = sedative_stop then charttime else null end)
+ min(starttime) != min(case when sedative_first = sedative_stop then starttime else null end)
 and
   max(sedative_rate) > 0 -- if the rate was always 0 or null, we consider it not a real drug delivery
 )
@@ -233,59 +227,59 @@ and
 , sedativecv_grp as
 (
 SELECT
-  s1.icustay_id,
+  s1.stay_id,
   s1.starttime,
   MIN(t1.endtime) AS endtime
 FROM sedativecv s1
 INNER JOIN sedativecv t1
-  ON  s1.icustay_id = t1.icustay_id
+  ON  s1.stay_id = t1.stay_id
   AND s1.starttime <= t1.endtime
   AND NOT EXISTS(SELECT * FROM sedativecv t2
-                 WHERE t1.icustay_id = t2.icustay_id
+                 WHERE t1.stay_id = t2.stay_id
                  AND t1.endtime >= t2.starttime
                  AND t1.endtime < t2.endtime)
 WHERE NOT EXISTS(SELECT * FROM sedativecv s2
-                 WHERE s1.icustay_id = s2.icustay_id
+                 WHERE s1.stay_id = s2.stay_id
                  AND s1.starttime > s2.starttime
                  AND s1.starttime <= s2.endtime)
-GROUP BY s1.icustay_id, s1.starttime
-ORDER BY s1.icustay_id, s1.starttime
+GROUP BY s1.stay_id, s1.starttime
+ORDER BY s1.stay_id, s1.starttime
 )
 -- now we extract the associated data for metavision patients
 -- do not need to group by itemid because we group by linkorderid
 , sedativemv as
 (
   select
-    icustay_id, linkorderid
+    stay_id, linkorderid
     , min(starttime) as starttime, max(endtime) as endtime
   from io_mv
-  group by icustay_id, linkorderid
+  group by stay_id, linkorderid
 )
 , sedativemv_grp as
 (
 SELECT
-  s1.icustay_id,
+  s1.stay_id,
   s1.starttime,
   MIN(t1.endtime) AS endtime
 FROM sedativemv s1
 INNER JOIN sedativemv t1
-  ON  s1.icustay_id = t1.icustay_id
+  ON  s1.stay_id = t1.stay_id
   AND s1.starttime <= t1.endtime
   AND NOT EXISTS(SELECT * FROM sedativemv t2
-                 WHERE t1.icustay_id = t2.icustay_id
+                 WHERE t1.stay_id = t2.stay_id
                  AND t1.endtime >= t2.starttime
                  AND t1.endtime < t2.endtime)
 WHERE NOT EXISTS(SELECT * FROM sedativemv s2
-                 WHERE s1.icustay_id = s2.icustay_id
+                 WHERE s1.stay_id = s2.stay_id
                  AND s1.starttime > s2.starttime
                  AND s1.starttime <= s2.endtime)
-GROUP BY s1.icustay_id, s1.starttime
-ORDER BY s1.icustay_id, s1.starttime
+GROUP BY s1.stay_id, s1.starttime
+ORDER BY s1.stay_id, s1.starttime
 )
 select
-  icustay_id
+  stay_id
   -- generate a sequential integer for convenience
-  , ROW_NUMBER() over (partition by icustay_id order by starttime) as sedativenum
+  , ROW_NUMBER() over (partition by stay_id order by starttime) as sedativenum
   , starttime, endtime
   , extract(epoch from endtime - starttime)/60/60 AS duration_hours
   -- add durations
@@ -295,12 +289,12 @@ from
 UNION
 
 select
-  icustay_id
-  , ROW_NUMBER() over (partition by icustay_id order by starttime) as sedativenum
+  stay_id
+  , ROW_NUMBER() over (partition by stay_id order by starttime) as sedativenum
   , starttime, endtime
   , extract(epoch from endtime - starttime)/60/60 AS duration_hours
   -- add durations
 from
   sedativemv_grp
 
-order by icustay_id, sedativenum;
+order by stay_id, sedativenum;
